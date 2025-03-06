8d73aec57f4fa905ac524e456726a8bd88e8a5ba
John_Mulish <Johnnorbert1151@gmail.com>
2025-03-06 23:36:05 +0000

    First release of hello world

---
+++ ReadME.md
@@ -0,0 +1,7 @@
+this file helps us to know the basics of java
+thanks.
+ilove you
+but 
+you are mad
+hello world
+let's us learn java
---
+++ hello.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+import java.util.Scanner;
+public class hello{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String Name;
+
+        System.out.println("Please Enter Your Name : ");
+        Name = scanner.nextLine();
+        System.out.println("Hello " + Name);
+    
+}
+}
