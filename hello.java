a4f4eb1ed31d08e44bd23f30262ae3a3cf125411
John_Mulish <Johnnorbert1151@gmail.com>
2025-03-08 16:40:22 +0000

    i added my name on the file

--- hello.java
+++ hello.java
@@ -4,10 +4,11 @@ public class hello{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String Name;
-
+        System.out.println("I am JOHN and I am behind this code😊");
         System.out.println("Please Enter Your Name : ");
         Name = scanner.nextLine();
         System.out.println("Hello " + Name);
+        scanner.close();
     
 }
 }
